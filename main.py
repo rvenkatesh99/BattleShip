fe47635f6fd759bcd447c241a7aae09aba921f16
Rasika Venkatesh <rvenkatesh@ucdavis.edu>
2020-01-29 20:00:32 -0800

    Editing main method

--- main.py
+++ main.py
@@ -5,8 +5,26 @@ if __name__ == '__main__':
     board_dim = 10
     for i in range(len(sys.argv)):
         print(i, sys.argv[i])
-    with open(sys.argv) as infile:
-        for line in infile:
+
+    with open(sys.argv[1]) as infile:
+        content = infile.readlines()
+        print(content)
+
+    temp_str = content[0]
+    type(temp_str)
+    temp_str = temp_str.strip()
+    nr, nc = temp_str.split(" ")
+    nr = int(nr)
+    nc = int(nc)
+
+    boat_dict = {}
+    for i in range(1,len(content)):
+        temp_str = content[i]
+        temp_str = temp_str.strip()
+        boat, length = temp_str.split(" ")
+        boat_dict[boat] = int(length)
+        print(boat_dict)
+
 
     if len(sys.argv) >= 2:
         board_dim = int(sys.argv[1])
