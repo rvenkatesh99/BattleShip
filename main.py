18f1827d66a522306b87a1913113209c88deea17
Rasika Venkatesh <rvenkatesh@ucdavis.edu>
2020-01-29 19:14:55 -0800

    Editing main method

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from BattleShip.game import Game
 
 if __name__ == '__main__':
     board_dim = 10
+    for i in range(len(sys.argv)):
+        print(i, sys.argv[i])
+    with open(sys.argv) as infile:
+        for line in infile:
+
     if len(sys.argv) >= 2:
-        board_dim = int(sys.argv[0])
+        board_dim = int(sys.argv[1])
 
     game = Game(board_dim)
     game.play()
